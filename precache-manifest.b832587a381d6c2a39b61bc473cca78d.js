f2ed6b49cc22fea3d7142443fdc4fdf153151a49
ajstylee <ap332276@gmail.com>
2019-12-21 18:03:14 +0530

    Updates

---
+++ precache-manifest.b832587a381d6c2a39b61bc473cca78d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1fb8eba9d51b97a0f493979ed77d530e",
+    "url": "/Robo-Friend/index.html"
+  },
+  {
+    "revision": "0d61b93b0eeb2a16c5da",
+    "url": "/Robo-Friend/static/css/main.987373a6.chunk.css"
+  },
+  {
+    "revision": "7355c14aaf62b74826b9",
+    "url": "/Robo-Friend/static/js/2.b02be2e4.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/Robo-Friend/static/js/2.b02be2e4.chunk.js.LICENSE"
+  },
+  {
+    "revision": "0d61b93b0eeb2a16c5da",
+    "url": "/Robo-Friend/static/js/main.586059cd.chunk.js"
+  },
+  {
+    "revision": "fc1f4697263be86d6c73",
+    "url": "/Robo-Friend/static/js/runtime-main.a03917ea.js"
+  }
+]);
